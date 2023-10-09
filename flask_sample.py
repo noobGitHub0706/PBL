49f23a42a11853fb1caaa69457efc03aea131091
Rin <solomun319@gmail.com>
2023-10-09 17:13:47 +0900

    flask追加

---
+++ flask_sample.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__, static_folder='.', static_url_path='')
+@app.route('/')
+def index():
+    return app.send_static_file('PBL.html')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return name
+
+app.run(port=8000, debug=True)
